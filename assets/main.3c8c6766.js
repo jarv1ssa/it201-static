aed1ab9eaad5f127f44a5ced44620fc09ecd2a85
jarv1ssa <91316997+jarv1ssa@users.noreply.github.com>
2021-12-06 19:27:28 +0300

    Update main.3c8c6766.js

--- assets/main.3c8c6766.js
+++ assets/main.3c8c6766.js
@@ -47,15 +47,15 @@ var y=Object.defineProperty;var x=(o,e,t)=>e in o?y(o,e,{enumerable:!0,configura
         <div class="row">
           <div class="col-12 md:col-6 home__left">
             <p class="home__left--title">
-              Your trusted network for doing good
+              Give a hand, be happy
             </p>
 
             <p class="home__left--subtitle">
-              Built by a nonprofit. Forever free.
+              Built with heart. Forever free.
             </p>
 
             <p class="home__left--description">
-              Explore, donate, and share. Make a difference today.
+              Choose the category you want to help, and discover nonprofits.
             </p>
           </div>
 
